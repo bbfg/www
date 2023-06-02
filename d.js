6b389d4c5a74595ceda6466a85ba34333ce999bd
bbfg <135301088+bbfg@users.noreply.github.com>
2023-06-02 12:39:06 +0800

    Add files via upload

---
+++ d.js
@@ -0,0 +1,3 @@
+document.writeln("<a href=\'//tool.chinaz.com\' class=\'ToolLogo fl\'>");
+document.writeln("                    <img src=\'//csstools.chinaz.com/tools/images/public/logos/logo-index.png\'>");
+document.writeln("                </a>");
